043622bfe61cc64e5b2d1f836dd0a486aa178d96
dharshan-kumarj <jdharshankumar18@gmail.com>
2024-06-08 17:10:33 +0530

    added OPenAI source

---
+++ include/AI/OpenAI/OpenAI.py
@@ -0,0 +1,31 @@
+from openai import OpenAI  #to use ai chatbot using API keys
+import os   
+from dotenv import load_dotenv  #to load the api key from .env
+
+
+
+
+user_input= input(">>")
+load_dotenv()  #unload the API keys from .env
+
+client = OpenAI(                            #from 17 to 35 it is for to generate ai answer
+    api_key=os.getenv("OPEN_API_KEY")
+)
+
+def structured_generator(prompt):
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": prompt
+            },
+        ],
+        model="gpt-3.5-turbo"
+    )
+    return chat_completion
+
+prompt = ( f" {user_input}")
+result=structured_generator(prompt)
+output=result.choices[0].message.content
+
+print(output)
